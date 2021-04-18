a989992202f8ef133b41128b4b47ff13f7f87cff
Mick <grevmivlos@gmail.com>
2021-04-18 21:20:14 +0200

    Build and deployed

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,18 @@
-export const URL = "http://localhost:8080/CA3_Boilerplate";
+const domain = "https://micklarsen.com/";
+/* const domain = "http://localhost:"; */
+
+const port = "8080/";
+const app = "Eksamen_Boilerplate_Backend-1/";
+
+
+/* LOCAL HOST DEV */
+/* export const URL = "http://localhost:8080/CA3_Boilerplate";
 export const URLJokes = "http://localhost:8080/CA3_Boilerplate/api/jokes";
 export const URLScrapeSequential = "http://localhost:8080/CA3_Boilerplate/api/scrape/sequential";
-export const URLScrapeParallel = "http://localhost:8080/CA3_Boilerplate/api/scrape/parallel";
+export const URLScrapeParallel = "http://localhost:8080/CA3_Boilerplate/api/scrape/parallel"; */
+
+/* LIVE ENVIRONMENT */
+export const URL = domain + app;
+export const URLJokes = domain + app + "api/jokes";
+export const URLScrapeSequential = domain + app + "api/scrape/sequential";
+export const URLScrapeParallel = domain + app + "api/scrape/parallel";
